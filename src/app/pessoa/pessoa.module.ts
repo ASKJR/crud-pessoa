3c0aeb838e54c4c5ee830bc561cbcec8717cbd24
Alberto Kato <albertokatojr@gmail.com>
2024-07-18 15:17:42 -0300

    Added directive numeric

--- src/app/pessoa/pessoa.module.ts
+++ src/app/pessoa/pessoa.module.ts
@@ -6,8 +6,14 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { InserirPessoaComponent } from './inserir-pessoa/inserir-pessoa.component';
 import { EditarPessoaComponent } from './editar-pessoa/editar-pessoa.component';
+import { NumericoDirective } from '../shared/directives/numerico.directive';
 @NgModule({
-  declarations: [ListarPessoaComponent, InserirPessoaComponent, EditarPessoaComponent],
+  declarations: [
+    ListarPessoaComponent,
+    InserirPessoaComponent,
+    EditarPessoaComponent,
+    NumericoDirective,
+  ],
   imports: [CommonModule, RouterModule, FormsModule],
   providers: [PessoaService],
 })
